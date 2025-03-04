8304334111bc7978adfd5585e096e3edffec1925
Uzair mirza <UZAIRMIRZA2121@GMAIL.COM>
2025-03-04 22:58:41 +0500

    Update header to include versioning for stylesheet to improve cache management

--- layouts/header.php
+++ layouts/header.php
@@ -8,7 +8,9 @@ require_once "../db.php";
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <!-- <link rel="stylesheet" href="../assets/css/style.css"> -->
+    <link rel="stylesheet" href="../assets/css/style.css?v=<?= filemtime('style.css'); ?>">
+
   </head>
   <body>
   <div class="mobile-wrap">
